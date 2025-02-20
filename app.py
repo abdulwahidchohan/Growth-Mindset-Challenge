7564966b9d3131f5465e9a69d1abf49eafd4e078
abdulwahidchohan <91005376+abdulwahidchohan@users.noreply.github.com>
2025-02-20 19:35:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import plotly.express as px
+import plotly.express as px # type: ignore
 import plotly.figure_factory as ff
 from sklearn.cluster import DBSCAN, KMeans
 import seaborn as sns
